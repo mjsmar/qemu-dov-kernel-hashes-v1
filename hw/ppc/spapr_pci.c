5feeda3541915a334c223477e89a294dbc53785e
Michael Roth <mdroth@linux.vnet.ibm.com>
2016-03-07 13:50:13 -0600

    *debug reg/assigned-addresses props

--- hw/ppc/spapr_pci.c
+++ hw/ppc/spapr_pci.c
@@ -846,6 +846,35 @@ typedef struct ResourceProps {
     uint32_t assigned_len;
 } ResourceProps;
 
+static void debug_resource_props(ResourceProps *rp)
+{
+    int i;
+
+    printf("reg fields:\n");
+
+    for (i = 0; i < rp->reg_len / sizeof(ResourceFields); i++) {
+        printf("%i: phys_{hi,mid,lo}: %xh %xh %xh, size_{hi,lo}: %xh %xh\n",
+               i,
+               be32_to_cpu(rp->reg[i].phys_hi),
+               be32_to_cpu(rp->reg[i].phys_mid),
+               be32_to_cpu(rp->reg[i].phys_lo),
+               be32_to_cpu(rp->reg[i].size_hi),
+               be32_to_cpu(rp->reg[i].size_lo));
+    }
+
+    printf("assigned fields:\n");
+
+    for (i = 0; i < rp->assigned_len / sizeof(ResourceFields); i++) {
+        printf("%i: phys_{hi,mid,lo}: %xh %xh %xh, size_{hi,lo}: %xh %xh\n",
+               i,
+               be32_to_cpu(rp->assigned[i].phys_hi),
+               be32_to_cpu(rp->assigned[i].phys_mid),
+               be32_to_cpu(rp->assigned[i].phys_lo),
+               be32_to_cpu(rp->assigned[i].size_hi),
+               be32_to_cpu(rp->assigned[i].size_lo));
+    }
+}
+
 /* fill in the 'reg'/'assigned-resources' OF properties for
  * a PCI device. 'reg' describes resource requirements for a
  * device's IO/MEM regions, 'assigned-addresses' describes the
@@ -950,6 +979,7 @@ static void populate_resource_props(PCIDevice *d, ResourceProps *rp)
 
     rp->reg_len = reg_idx * sizeof(ResourceFields);
     rp->assigned_len = assigned_idx * sizeof(ResourceFields);
+    debug_resource_props(rp);
 }
 
 typedef struct PCIClass PCIClass;
